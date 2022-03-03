7a1162c31511525dcd80e80432e0b51d3495414c
chantszf <chantszf@sheridancollege.ca>
2022-03-03 14:11:39 -0500

    PokerDeck and PokerCards' basic function created

--- src/main/java/syst17796projectgofish/Poker/PokerCards.java
+++ src/main/java/syst17796projectgofish/Poker/PokerCards.java
@@ -2,8 +2,68 @@ package syst17796projectgofish.Poker;
 
 /**
  *
- * @author ********
+ * @author Richie Chan
  */
-public class PokerCards {
+public class PokerCards implements Comparable<PokerCards> {
     
+    //Empty constructor
+    protected PokerCards(){}
+
+    //Enum of Card Values
+    protected enum VALUE {
+        TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11), QUEEN(12), KING(13), ACE(14);
+
+        private int ord;
+        private VALUE(int i){
+            this.ord = i;
+        }
+        public int getOrd(){
+            return ord;
+        }
+    }
+
+    //Enum of Card Suits
+    protected enum SUIT {CLUB, DIAMOND, HEARTS, SPADE}
+
+    private VALUE value;
+    private SUIT suit;
+
+    //Construct a PokerCard Object
+    protected PokerCards(VALUE value, SUIT suit){
+        this.value = value;
+        this.suit = suit;
+    }
+
+    //Get the value of the PokerCard Object
+    protected int getValue() {
+        return value.getOrd();
+    }
+    protected void setValue(VALUE value) {
+        this.value = value;
+    }
+    protected SUIT getSuit() {
+        return suit;
+    }
+    protected void setSuit(SUIT suit) {
+        this.suit = suit;
+    }
+
+    //Compare cards value, return 0 if the same, 1 if larger
+    @Override
+    public int compareTo(PokerCards o) {
+        if(this.getValue() == o.getValue()){
+            return 0;
+        }
+        else if(this.getValue() > o.getValue()) {
+            return 1;
+        }
+        else
+            return -1;
+    }
+
+    //toString method of Card
+    @Override
+    public String toString() {
+        return suit + " OF " + value;
+    }
 }
--- src/main/java/syst17796projectgofish/Poker/PokerDeck.java
+++ src/main/java/syst17796projectgofish/Poker/PokerDeck.java
@@ -1,9 +1,47 @@
 package syst17796projectgofish.Poker;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+
 /**
  *
- * @author ********
+ * @author Richie Chan
  */
-public class PokerDeck {
+public class PokerDeck extends PokerCards {
     
+    //a list of PokerCards is a Poker Deck
+    public List<PokerCards> pokerDeck = new ArrayList<>();
+    
+    //Poker Deck constructor generate a full deck of Poker Cards
+    public PokerDeck() {
+        for(SUIT suit : SUIT.values()){
+            for(VALUE value : VALUE.values()){
+                PokerCards cd = new PokerCards(value, suit);
+                pokerDeck.add(cd);
+            }
+        }
+    }
+    
+    //Get the cards at the index i of the Poker Deck
+    public PokerCards getPokerCards(int i) {
+        return pokerDeck.get(i);
+    }
+    
+    //Shffle Deck function
+    public void shuffleCards(){
+        Collections.shuffle(this.pokerDeck);
+    }
+    
+    //List out all the cards from top to bottom of the deck
+    @Override
+    public String toString() {
+        String output = "";
+        ListIterator<PokerCards> i = this.pokerDeck.listIterator();
+        while(i.hasNext()){
+            output += i.next() + ". ";
+        }
+        return output;
+    }
 }
